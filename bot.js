88837d6f2a0f50bf45c052f46be7c996f6b732bf
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-28 12:47:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const TelegramBot = require("node-telegram-bot-api");
 //const checker = require('./index.js');
 const { checker } = require('./index')
 
-const token = "2054859695:AAGVSXp1MRtrAMP0L5g2AML-tBVvwRfxi4o"
+require('dotenv').config();
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(process.env.DFA_CMD, {polling: true});
 
 
 bot.onText(/\/start/, (msg) => {
