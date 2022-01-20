5249bcae47211821bb1970fc223781350423687e
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-01-20 13:52:14 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { firefox } = require("playwright-firefox");
 const { send_log, send_notif} = require('./telegram.js');
 
-var OK = '\x1b[33m%s\x1b[0m';
-var BAD = '\x1b[31m%s\x1b[0m';
+// var OK = '\x1b[33m%s\x1b[0m';
+// var BAD = '\x1b[31m%s\x1b[0m';
 var on_task = true
 var choice = null
 var client_count = ""
@@ -23,7 +23,7 @@ class checker{
                 // Check input[type="checkbox"]
                 await page.check('input[type="checkbox"]');
                 send_log("Step 1....Passed");
-                console.log(OK,"Step 1....Passed");
+                // console.log(OK,"Step 1....Passed");
                 if(choice == 1){
                   // Click text=Start Individual Appointment
                   await page.click('text=Start Individual Appointment');
@@ -32,29 +32,29 @@ class checker{
                   // Click text=Start Group Appointment
                   await page.click('text=Start Group Appointment');
                   await page.waitForTimeout(2000);
-                  console.log(client_count)
+                  // console.log(client_count)
                   await page.selectOption('#numberOfApplicants' , {'value': String(client_count)});
                   await page.click('text=Next');
                 }
                 send_log("Step 2....Passed");
-                console.log(OK,"Step 2....Passed");
+                // console.log(OK,"Step 2....Passed");
                 await page.waitForTimeout(2000);
                 //await page.click('#SiteID')
                 await page.selectOption('#SiteID' , {'index':1});
                 send_log("Step 3....Passed");
-                console.log(OK,"Step 3....Passed");
+                // console.log(OK,"Step 3....Passed");
                 //assert.equal(page.url(), 'https://www.passport.gov.ph/appointment/individual/site');
                 await page.waitForTimeout(1000);
                 // Click #pubpow-notif >> text=Please check if you agree
                 //await page.check('input[name="pubpow-notif-checkbox"]');
                 await page.click('label[for="pubpow-notif-checkbox"]');
                 send_log("Step 4....Passed");
-                console.log(OK,"Step 4....Passed");
+                // console.log(OK,"Step 4....Passed");
               
                 // Click text=Next
                 await page.click('text=Next');
                 send_log("Step 5....Passed");
-                console.log(OK,"Step 5....Passed");
+                // console.log(OK,"Step 5....Passed");
                        
                 await page.isHidden('.oas-loading')
 
@@ -74,11 +74,11 @@ class checker{
                         var branch_name = await page.$eval('#SiteID', sel => sel.options[sel.options.selectedIndex].textContent)
                         if(available_date.includes("No available date")){
                           send_log(`NO APPOINTMENT AVAILABLE\n\n${branch_name}\n\n\nStatus: ${available_date}\n\n${date}`)
-                            console.log(BAD,`NO APPOINTMENT AVAILABLE IN ${branch_name}`)
+                            // console.log(BAD,`NO APPOINTMENT AVAILABLE IN ${branch_name}`)
                         }
                         else{
                           send_notif(`APPOINTMENT AVAILABLE!!\n\n${branch_name}\n\n\nAvailable date: ${available_date}\n\n${date}`)
-                          console.log(OK,`APPOINTMENT AVAILABLE IN ${branch_name} DATE: ${available_date}}`)
+                          // console.log(OK,`APPOINTMENT AVAILABLE IN ${branch_name} DATE: ${available_date}}`)
                         }
                         await page.selectOption('select#SiteID', {'index' : element});
                   }
