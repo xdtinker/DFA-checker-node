d5714d36c9e1a04f1cf38a113b544f4deab52817
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-02-16 11:02:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,86 @@
-const { firefox } = require("playwright-firefox");
+const { firefox, devices } = require('playwright-firefox');
 const { send_log, send_notif } = require('./telegram.js');
+const device = devices['iPad Pro 11'];
 
-var OK = '\x1b[33m%s\x1b[0m';
-var BAD = '\x1b[31m%s\x1b[0m';
-var on_task = true
-var choice = null
-var client_count = ""
-class checker {
-    static async run() {
-        (async() => {
-            const browser = await firefox.launch({
-                headless: true
-            });
-            const context = await browser.newContext();
-            // Open new page
-            const page = await context.newPage();
-            if (!on_task) on_task = true
-            try {
-                await page.goto('https://www.passport.gov.ph/appointment');
-                send_log("Running System checks.");
-                await page.waitForTimeout(2000);
-                // Check input[type="checkbox"]
-                await page.check('input[type="checkbox"]');
-                send_log("1/5....Passed");
-                console.log(OK, "1/5....Passed");
-                if (choice == 1) {
-                    // Click text=Start Individual Appointment
-                    await page.click('text=Start Individual Appointment');
-                } else {
-                    // Click text=Start Group Appointment
-                    await page.click('text=Start Group Appointment');
-                    await page.waitForTimeout(2000);
-                    // console.log(client_count)
-                    await page.selectOption('#numberOfApplicants', { 'value': String(client_count) });
-                    await page.click('text=Next');
-                }
-                send_log("2/5....Passed");
-                console.log(OK, "2/5....Passed");
-                await page.waitForTimeout(2000);
-                //await page.click('#SiteID')
-                await page.selectOption('#SiteID', { 'index': 1 });
-                send_log("3/5....Passed");
-                console.log(OK, "3/5....Passed");
-                //assert.equal(page.url(), 'https://www.passport.gov.ph/appointment/individual/site');
-                await page.waitForTimeout(2000);
-                // Click #pubpow-notif >> text=Please check if you agree
-                //await page.check('input[name="pubpow-notif-checkbox"]');
-                await page.click('label[for="pubpow-notif-checkbox"]');
-                send_log("4/5....Passed");
-                console.log(OK, "4/5....Passed");
+(async() => {
+    const browser = await firefox.launch({
+        headless: true
+    })
+    const context = await browser.newContext({
+        locale: 'en-PH',
+        timezoneId: 'Asia/Manila',
+        viewport: device.viewport,
+        userAgent: device.userAgent,
+    })
 
-                // Click text=Next
-                await page.click('text=Next');
-                send_log("5/5....Passed");
-                console.log(OK, "5/5....Passed");
+    const page = await context.newPage()
+    try {
+        isTrue = true
+        arr = [0, 1, 4]
+        while (isTrue) {
+            for await (const i of arr) {
+                await page.goto('https://co.dfaapostille.ph/dfa/')
+
+                await page.waitForTimeout(1000)
+
+                await page.click('text=SCHEDULE AN APPOINTMENT')
+
+                await page.click('[data-dismiss="modal"] >> nth=1')
+
+                await page.locator('#Email').fill('aziz.saricula@gmail.com')
+                await page.locator('#Password').fill('Anon123s.')
+
+                await page.click('text=LOGIN')
+                await page.click('#declaration-agree')
+                await page.click('#terms-and-conditions-agree')
+
+                //Home page
+                // await page.waitForTimeout(1000)
+                await page.click('#show-document-owner')
+
+                // await page.waitForTimeout(1000);
+                //0, 1, 4
+                await page.selectOption('#site', { 'index': i })
+                await page.click('#stepSelectProcessingSiteNextBtn')
 
 
-                
-                await page.isHidden('.oas-loading')
+                //Document owner   
+                await page.locator('#Record_FirstName').fill('Datu Abdulaziz')
+                await page.locator('#Record_MiddleName').fill('Matabalao')
+                await page.locator('#Record_LastName').fill('Saricula')
+                await page.locator('#Record_DateOfBirth').fill('2000-02-25') // birthdate format (yyyy-mm-dd)
+                await page.locator('#Record_ContactNumber').fill('9954200802')
+                await page.locator('#Record_CountryDestination').selectOption({ 'value': 'Kuwait (KWT)' })
 
-                await page.waitForTimeout(2000)
-                
-                send_log("You will be notified when there is available appointment at @DFAPassport_bot");
-                
-                var branch_index = [3, 4, 7, 24, 27, 31]
-                //var branch_index = [23, 43]
+                await page.locator('#documentsSelectionBtn').click()
+                await page.locator('#nbiClearance').click()
+                await page.locator('#qtyNbiClearanceRegular').fill('1')
+                await page.locator('#selectDocumentsBtn').click()
 
-                let kill_count = 0
-                while (true) {
-                    if (on_task === false) break
-                    
-                    for (const element of branch_index) {         
-                        
-                        kill_count += .5
-                        if (kill_count >= 3000) throw Error("Time limit Exceeded, Dyno will restart \nUse /start command to restart task")
-                        
-                        await page.selectOption('select#SiteID', { 'index': element });
-                        var date = new Date().toLocaleString().toUpperCase();
-                        await page.waitForTimeout(500);
-                        //await page.waitForSelector("#next-available-date")
-                        var available_date = await page.$eval("#next-available-date", date_status => date_status.textContent)
-                        var branch_name = await page.$eval('#SiteID', sel => sel.options[sel.options.selectedIndex].textContent)
-                        if (available_date.includes("No available date")) {
-                            // send_log(`NO APPOINTMENT AVAILABLE\n\n${branch_name}\n\n\nStatus: ${available_date}\n\n${date}`)
-                            console.log(`NO APPOINTMENT AVAILABLE IN ${branch_name}`)
-                        } else {
-                            send_notif(`APPOINTMENT AVAILABLE!!\n\n${branch_name}\n\n\nAvailable date: ${available_date}\n\n${date}`)
-                            console.log(`APPOINTMENT AVAILABLE IN ${branch_name} DATE: ${available_date}}`)
-                        }
+                await page.locator('#stepOneNextBtn').click()
+
+
+                const available_date = await page.$$('div[class="fc-content"]');
+                var branch_name = await page.$eval("#siteAndNameAddress", bname => bname.textContent)
+                for await (const dates of available_date) {
+                    if (await dates.innerText() == 'Not Available') {
+                        // console.log(await dates.innerText());
+                        console.log(`NO APPOINTMENT FOUND IN ${branch_name}`)
+                    } else {
+                        send_notif(`APPOINTMENT FOUND IN ${branch_name}`)
+                        isTrue = false
+                        break
                     }
                 }
-            } catch (e) {
-                send_log(e.message)
-                console.log(e)
-            } finally {
-                console.log('Exit Status: 1')
-                await context.close();
-                await browser.close();
-                process.exit(0)
+                await page.click('.float-right')
             }
-        })();
+        }
+    } catch (e) {
+        console.log(e);
+        send_notif(e)
+    } finally {
+        await context.close()
+        await browser.close()
+        send_notif('task ended')
     }
-
-    static stop() {
-        on_task = false
-    }
-    static individual() {
-        choice = 1
-    }
-    static group(count) {
-        choice = 2
-        client_count = count
-    }
-}
-
-module.exports = { checker }
+})()
